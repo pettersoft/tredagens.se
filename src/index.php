d2a5abb4d647d66ac003cdb87e9cb5b5062774ce
Petter Pettersson <pettersoft@gmail.com>
2025-11-29 17:26:06 +0100

    mnenu

--- src/index.php
+++ src/index.php
@@ -1,6 +1,12 @@
 <?php
 require_once __DIR__ . '/template/template_top.php';
 ?>
+<div class="content">
+    <h1>Tre dagens</h1>
+    <p>Välkommen till Tre Dagens officiella hemsida! Vi är Enångers största (till ytan) coverband och vi älskar att få folk att dansa och ha kul. Oavsett om det är en bröllopsfest, företagsevent eller en kväll på lokalpuben, så levererar vi alltid en energifylld show med låtar som alla känner igen.</p>
+    <p>"Spela Creedence annars slår jag ihjäl dig!" - Vi lever än, vi levererar alltid Creedence.</p>
+    <p><em>PS. Vi erbjuder även Quiz! Kontakta oss för mer information och bokning.</em></p>
+</div>
 <?php
 require_once __DIR__ . '/template/template_bot.php';
 ?>
