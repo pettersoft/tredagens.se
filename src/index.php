a1c5f5781e732b1613f6cb29c569a79119e60e44
Petter Pettersson <pettersoft@gmail.com>
2025-12-07 21:11:15 +0100

    added more

--- src/index.php
+++ src/index.php
@@ -2,45 +2,62 @@
 require_once __DIR__ . '/template/template_top.php';
 ?>
 <section class="page bg1">
-<header>
-    <div class="header-wrapper">
-    <img src="/static/img/logo_white.png" alt="Tre Dagens logga">
-</div>
-</header>
+    <header>
+        <div class="header-wrapper">
+            <img src="/static/img/logo_white.png" alt="Tre Dagens logga">
+        </div>
+    </header>
 
 
     <div class="content">
         <h1>Tre dagens</h1>
-        <p>Välkommen till Tre Dagens officiella hemsida! Vi är Enångers största (till ytan) band och vi älskar att få folk att dansa och ha kul. Oavsett om det är en 50-årsfest, kräftskiva eller något annat värt att fira, så levererar vi alltid en energifylld show med låtar som alla känner igen.</p>
+        <p>Välkommen till Tre Dagens officiella hemsida! Vi är Enångers största (till ytan) band och vi älskar att få
+            folk att dansa och ha kul. Oavsett om det är en 50-årsfest, kräftskiva eller något annat värt att fira, så
+            levererar vi alltid en energifylld show med låtar som alla känner igen.</p>
         <p>"Spela Creedence annars slår jag ihjäl dig!" - Vi lever än, vi levererar alltid Creedence.</p>
         <p><em>PS. Vi erbjuder även Quiz! Kontakta oss för mer information och bokning.</em></p>
     </div>
 </section>
 
-<section class="page">
-    <div class="content">
-        <h2>Vilka är vi?</h2>
-        <div class="band-members">
-            <div class="band-member">
-                <h3>Petter Pettersson</h3>
-                <p>Gitarr</p>
-                <img src="/static/img/petter.jpg" alt="Bild på Petter Pettersson">
+<section class="band-container">
+    <h2>Vilka är vi?</h2>
+    <div class="band-members">
+        <div class="band-member">
+            <div class="band-member-img">
+                <img src="/static/img/petter.png" alt="Bild på Petter Pettersson" loading="lazy">
             </div>
-
-            <div class="band-member">
-                <h3>Ivar Engnell</h3>
-                <p>Sång & Gitarr</p>
-                <img src="/static/img/ivar.jpg" alt="Bild på Ivar Engnell">
+            <div class="band-member-info">
+                <h3>Petter Pettersson</h3>
+                <h4>Gitarr</h4>
+                <p>Förutom att spela gitarr är Petter även vass i studion och har producerat flera låtar. Hans favoritband är The Who och det kan bjudas på windmills på scenen!</p>
             </div>
         </div>
-    </div>
 
-<section class="page">
-    <div class="content-white">
-        <h2>Kontakta oss</h2>
-        <p>För bokningar och förfrågningar, vänligen kontakta oss via e-post på <a href="mailto:pettersoft@gmail.com">pettersoft@gmail.com</a>
-        eller ring oss på <a href="tel:+46725240777">072-52 40 777</a>. Vi ser fram emot att höra från dig och göra ditt evenemang oförglömligt!</p>
+        <div class="band-member">
+            <div class="band-member-info">
+                <h3>Ivar Engnell</h3>
+                <h4>Sång, gitarr och stomp</h4>
+                <p>Det är inte bara musik, det är en livsstil. Ivar, spelman i grunden, är förutom en riktigt bra
+                    vokalist, även riktigt vass på dragspel och folkmusik.</p>
+            </div>
+            <div class="band-member-img">
+                <img src="/static/img/ivar.png" alt="Bild på Ivar Engnell" loading="lazy">
+            </div>
+        </div>
+
+
     </div>
-<?php
+    </div>
+</section>
+
+<section class="page bg1" style="padding-top: 5rem">
+    <div class="content">
+        <h2>Kontakta oss</h2>
+        <p>För bokningar och förfrågningar, vänligen kontakta oss via e-post på <a
+                href="mailto:pettersoft@gmail.com">pettersoft@gmail.com</a>
+            eller ring oss på <a href="tel:+46725240777">072-52 40 777</a>. Vi ser fram emot att höra från dig och göra
+            ditt evenemang oförglömligt!</p>
+    </div>
+    <?php
 require_once __DIR__ . '/template/template_bot.php';
 ?>
