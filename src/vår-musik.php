0f422a04b45891f47a031103b53616009c63e1bc
Petter Pettersson <pettersoft@gmail.com>
2026-08-20 21:34:19 +0200

    Remove vår musik thingy

--- src/vår-musik.php
+++ src/vår-musik.php
@@ -17,10 +17,6 @@ require_once __DIR__ . '/template/template_top.php';
         <h1>Vår musik</h1>
         <p>När vi spelar live så kan ni räkna med Mando Diao, Creedence, Chuck Berry, Eric Clapton, Bob Dylan, Eddie Cochran, Elvis & Johnny Cash, med mera!</p>
         <p>Vi uppdaterar ofta vår setlist och brukar kunna skräddarsy efter vem det är som anordnar!</p>
-
-        <h2>Men "er" musik då?</h2>
-        <p>Givetvis! Vi skriver också egen musik. Några av våra främst alster faller inom genren gypsy jazz, detta är vår gode vän, Lars Gillman Jonsson, favoritlåt!</p>
-        <iframe width="300" height="315" src="https://www.youtube.com/embed/3a2IwAtyhCQ?si=b3TMmLC-QkdZh5Gz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
 </section>
 
